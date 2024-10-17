91ce9bf2fc7efc0805ae236f957a5aca6dd7d9d3
FarieB <faraibande@gmail.com>
2024-10-17 20:28:16 +0000

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        # Check if n is divisible by factor
+        while n % factor == 0:
+            operations += factor  # Add the factor to operations
+            n //= factor  # Reduce n by the factor
+        factor += 1
+
+    return operations
