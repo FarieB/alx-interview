e9a67eefcead8f3c6ddf02848fc3604910f3472e
FarieB <133366471+FarieB@users.noreply.github.com>
2024-10-23 11:51:34 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
+
+""" script that reads stdin line by line and computes metrics """
+
 import sys
 
-# Initializes variables to store total file size and status code counts
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-def print_stats():
-    """Print the accumulated statistics."""
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
-    line_count = 0
     for line in sys.stdin:
-        # Splits the line and extract the file size and status code
-        parts = line.split()
-        if len(parts) > 6:
-            try:
-                status_code = int(parts[-2])
-                file_size = int(parts[-1])
-                
-                # Updates total file size
-                total_size += file_size
-                
-                # Updates status code count if it's valid
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                
-                line_count += 1
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-                # Prints stats every 10 lines
-                if line_count % 10 == 0:
-                    print_stats()
+        stlist = line.split()
+        count += 1
 
-            except (ValueError, IndexError):
-                # Skips the line if there's an error in conversion
-                continue
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
 except KeyboardInterrupt:
-    # Prints stats on keyboard interruption (Ctrl + C)
-    print_stats()
+    printsts(sts, size)
     raise
