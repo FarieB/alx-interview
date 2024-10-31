f874c0b2a333f91b9290643e654387acc32af0f3
FarieB <faraibande@gmail.com>
2024-10-31 20:07:40 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+data set represents a valid UTF-8 encoding
+"""
+
+def validUTF8(data):
+    # Number of bytes left to process in the current character
+    num_bytes = 0
+
+    # Masks to check the most significant bits
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    for byte in data:
+        # Only interested in the 8 least significant bits
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            # Determine the number of bytes in this character
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+
+            # 1-byte character (ASCII) or invalid
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            # Check that this byte has the form 10xxxxxx
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
